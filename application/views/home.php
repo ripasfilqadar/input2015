2a1b2eb878b932f0a160d7f3b3a0ead7670e44e7
adhipoer <putu.adhi.purwanto@gmail.com>
2015-06-24 04:15:10 +0700

    untuk smk

--- application/views/home.php
+++ application/views/home.php
@@ -21,7 +21,7 @@
 
                     <?php
                         if(substr($tingkatan,0,3)=='smk') 
-                        echo "<div style='background-color:#2ecc71; border:1px solid #66CE5B; display:block; text-align:center;width:92%; margin:14px 0 14px 0; padding:12px 10px 12px 10px; color:white;'> Form pengisian nilai hasil tes SMK dapat diunduh <a href=".base_url()."format/".str_replace(" ","%20",$this->session->userdata('NAMA_SEKOLAH')).".xlsx>". "<span style='font-style:italic; font-weight:bold;font-size:16px;'>disini</span> </a> </div>";?>
+                        echo "<div class='blink' style='background-color:#2ecc71; border:1px solid #66CE5B; display:block; text-align:center;width:92%; margin:14px 0 14px 0; padding:12px 10px 12px 10px; color:white;'> Form pengisian NTMB dan NTK untuk SMK dapat diunduh <a href=".base_url()."format/".str_replace(" ","%20",$this->session->userdata('NAMA_SEKOLAH')).".xlsx>". "<span style='font-style:italic; font-weight:bold;font-size:16px;'>disini</span> </a> </div>";?>
 
                     <?php 
                         // if(substr($tingkatan,0,3)=='smp')echo  "<p style='background-color:yellow; border:1px solid #66CE5B; display:block; text-align:center; width:92%; margin:14px 0 14px 0; padding:12px 10px 12px 10px;'>Petugas Input Data Pendaftaran SMP diwajibkan melakukan pengecekan Nilai Sekolah Bahasa Indonesia, Matematika, dan IPA yang tertera pada SKL/SKHUN dan Rata-Rata Nilai Rapor Bahasa Indonesia, Matematika, dan IPA </p>"; 
